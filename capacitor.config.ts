d3e0d8a74b20cd98eeca45b831d9c6bc53c1d016
Juan Pablo Jaramillo Orrego <juan.jaramillo@pragma.com.co>
2024-11-28 06:21:19 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '02_shopping_list_app',
+  webDir: 'www'
+};
+
+export default config;
